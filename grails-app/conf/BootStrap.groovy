01c61c4960536db5498dabb7e368c764dad4aa50
Kukagres <Kukagres@Kovogitek.labs>
2014-06-30 21:20:51 -0300

    Avances en layout dinamico.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,36 @@
-class BootStrap {
+import app.Grupo
+import app.Usuario
 
-    def init = { servletContext ->
+
+class BootStrap
+{
+    def init = { servletContext ->   
+            new Grupo
+            (
+                nombre:'publico',
+                isAdmin:false
+            ).addToUsuarios(new Usuario
+            (
+                  nombre:'Jonatan',
+                  apellido:'Quintero',
+                  password: "12345678",
+                  dni: 36303191,
+                  email: 'joni1692@hotmail.com'
+            )).save()
+            new Grupo
+            (
+                nombre:'Administradores',
+                isAdmin:true
+            ).addToUsuarios(new Usuario
+            (
+                  nombre:'Kukagres',
+                  apellido:'Salkute',
+                  password: "1234567",
+                  dni: 38087818,
+                  email: 'kukagres07@hotmail.com'
+            )).save()
     }
+    
     def destroy = {
     }
 }
